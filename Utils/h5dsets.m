d6c0c343b3467839581d9a5f84c0d034b07f5aef
Josh Laughner <joshlaugh5@gmail.com>
2014-02-27 14:36:24 -0800

    Changed 'h5dsets' to allow the user to enter the he5 group of interest or to use similar syntax to h5dsetname, where group numbers are specified as input arguments.

--- Utils/h5dsets.m
+++ Utils/h5dsets.m
@@ -1,6 +1,18 @@
-function [ dsets ] = h5dsets( h5group )
+function [ dsets ] = h5dsets( varargin )
 %h5dsets will iterate through the names of each data set in the group
 %passed to the function and return a cell structure with those names
+
+if nargin == 1 %If there's only one argument, assume that it is the group we want to enumerate the datasets for
+    h5group = varargin{1};
+    
+else
+    h5group = varargin{1}; %Save the h5 object (info or group)
+    n = length(varargin) - 1;
+    groups = cell2mat(varargin(2:end));
+    for i=1:n
+        h5group = h5group.Groups(groups(i));
+    end
+end
 
 n = length(h5group.Datasets);
 dsets = cell(n,1);
@@ -9,5 +21,4 @@ for i = 1:n
     dsets(i) = {info};
 end
 
-end
-
+end 
