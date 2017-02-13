c6c58240bb51c36ef3a042b78a1e7b4d15f6b6f9
Josh Laughner <joshlaugh5@gmail.com>
2017-02-13 09:43:27 -0800

    Some utility changes.
    MODIFIED FILE:
        Utils/h5dsets.m - can now format the output so that it can be used programmatically.
    
    NEW FILE:
        Utils/load_behr_file.m - shortcut to load BEHR file for given date.

---
+++ Utils/load_behr_file.m
@@ -0,0 +1,18 @@
+function [ varargout ] = load_behr_file( file_date )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+
+behr_file = fullfile(BEHR_paths('behr_mat_dir'), behr_filename(file_date));
+D = load(behr_file);
+if nargout == 0
+    Data = D.Data;
+    OMI = D.OMI;
+    putvar(Data,OMI);
+else
+    varargout{1} = D.Data;
+    varargout{2} = D.OMI;
+end
+
+end
+
