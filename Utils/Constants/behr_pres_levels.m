e2021de097bc05f685d95ccccba6661baa05e5f5
Josh Laughner <joshlaugh5@gmail.com>
2017-04-12 13:40:20 -0700

    Added behr_pres_levels.m

---
+++ Utils/Constants/behr_pres_levels.m
@@ -0,0 +1,7 @@
+function [ pressure ] = behr_pres_levels(  )
+%BEHR_PRES_LEVELS Return the standard pressure levels used in the BEHR retrieval
+
+pressure = [1020 1015 1010 1005 1000 990 980 970 960 945 925 900 875 850 825 800 770 740 700 660 610 560 500 450 400 350 280 200];
+
+end
+
