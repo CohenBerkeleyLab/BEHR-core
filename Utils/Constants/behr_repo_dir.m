7ba33e6ff8cdb772782b228ea21ee3b789a2033b
Josh Laughner <joshlaugh5@gmail.com>
2017-09-14 12:56:30 -0700

    Deprecation warning added to behr_repo_dir

--- Utils/Constants/behr_repo_dir.m
+++ Utils/Constants/behr_repo_dir.m
@@ -9,6 +9,8 @@ function [ bdir ] = behr_repo_dir(  )
 %   repository; a warning will be issued if this does not appear to be the
 %   case. You should check the path it returns in that case.
 
+warning('BEHR_REPO_DIR has been deprecated in favor of behr_paths.behr_core');
+
 mydir = fileparts(mfilename('fullpath'));
 %Assumes this is in the Utils/Constants directory within the BEHR repo
 if isempty(regexp(mydir,'Utils/Constants$','once'))
